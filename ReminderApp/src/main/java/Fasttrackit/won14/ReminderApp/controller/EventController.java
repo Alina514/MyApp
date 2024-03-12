e332d356f49a649d58b0c59027a73f2ffe861f8b
Alina514 <148796255+Alina514@users.noreply.github.com>
2024-03-12 13:27:59 +0000

    Update EventController.java

--- ReminderApp/src/main/java/Fasttrackit/won14/ReminderApp/controller/EventController.java
+++ ReminderApp/src/main/java/Fasttrackit/won14/ReminderApp/controller/EventController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 @RestController
+@RequestMapping("events")
 public class EventController {
     @Autowired
     private EventService eventService;
